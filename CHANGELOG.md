76623f96b0a5720c2e85b313478d07d48281efb2
David Ramirez <david.ramirez@blueflag.com.au>
2025-01-23 21:05:34 +0800

    amend: cors features

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,15 @@
-## 0.2.0 - [01-14-2025] - davidlramirez95
+## 0.3.0 - [2025-01-23] - davidlramirez95
+### Added
+- Added the resource creation of the CORS module with different resource name
+- Added `.gitignore`
+
+### Removed
+- Removed `squidfunk/terraform-aws-api-gateway-enable-cors` module
+
+## 0.2.0 - [2025-01-14] - davidlramirez95
 ### Updated
 - Updated `x-blueflag-service-name` to `X-Blueflag-Service-Name`
 
-## 0.1.0 - [01-14-2025] - davidlramirez95
+## 0.1.0 - [2025-01-14] - davidlramirez95
 ### Added
 - API Gateway cors module
--- aws/api-gateway/cors/main.tf
+++ aws/api-gateway/cors/main.tf
@@ -1,14 +1,70 @@
-module "cors" {
-  source = "squidfunk/api-gateway-enable-cors/aws"
-  version = "0.3.3"
-  allow_headers = [
-    "Authorization",
-    "Content-Type",
-    "X-Amz-Date",
-    "X-Amz-Security-Token",
-    "X-Api-Key",
-    "X-Blueflag-Service-Name"
+# aws_api_gateway_method.method_options
+resource "aws_api_gateway_method" "method_options" {
+  rest_api_id   = var.api_id
+  resource_id   = var.api_resource_id
+  http_method   = "OPTIONS"
+  authorization = "NONE"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+# aws_api_gateway_integration.api_integration
+resource "aws_api_gateway_integration" "api_integration" {
+  rest_api_id = var.api_id
+  resource_id = var.api_resource_id
+  http_method = aws_api_gateway_method.method_options.http_method
+  content_handling = "CONVERT_TO_TEXT"
+
+  type = "MOCK"
+
+  request_templates = {
+    "application/json" = "{ \"statusCode\": 200 }"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+# aws_api_gateway_integration_response.api_integration_response
+resource "aws_api_gateway_integration_response" "api_integration_response" {
+  rest_api_id = var.api_id
+  resource_id = var.api_resource_id
+  http_method = aws_api_gateway_method.method_options.http_method
+  status_code = 200
+
+  response_parameters = local.integration_response_parameters
+
+  depends_on = [
+    aws_api_gateway_integration.api_integration,
+    aws_api_gateway_method_response.api_gateway_method_response,
   ]
-  api_id          = var.api_id
-  api_resource_id = var.api_resource_id
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+# aws_api_gateway_method_response.api_gateway_method_response
+resource "aws_api_gateway_method_response" "api_gateway_method_response" {
+  rest_api_id = var.api_id
+  resource_id = var.api_resource_id
+  http_method = aws_api_gateway_method.method_options.http_method
+  status_code = 200
+
+  response_parameters = local.method_response_parameters
+
+  response_models = {
+    "application/json" = "Empty"
+  }
+
+  depends_on = [
+    aws_api_gateway_method.method_options,
+  ]
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
